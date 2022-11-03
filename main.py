0050f7534219255a5d583f7c3190d8a7fd2c7198
2pokek <alkuzmich6479@gmail.com>
2022-11-03 22:48:10 +0300

    3

--- main.py
+++ main.py
@@ -1,27 +1,87 @@
-from flask import Flask,render_template, request, redirect
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from models import db,Reviews
+from models import db, Reviews
 
-
-app=Flask(__name__)
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3.db'
 
 db.init_app(app)
 
+
+@app.route('/create')
+def create():
+    db.create_all()
+    return 'all tables created'
+
+
 @app.route('/')
 def main_page():
-        return render_template('main_page.html')
+    return render_template('main_page.html')
+
 
 @app.route('/pricing')
 def pricing_page():
-        return render_template('pricing.html')
+    return render_template('pricing.html')
+
 
 @app.route('/about')
 def about_page():
-        return render_template('about.html')
+    return render_template('about.html')
+
+
+@app.route('/create_comment', methods=['POST', 'GET'])
+def create_comment():
+    if request.method == "POST":
+        nickname = request.form['nickname']
+        text = request.form['text']
+
+        review = Reviews(nickname=nickname, text=text)
+
+        try:
+            db.session.add(review)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error, try again later'
+    else:
+        return render_template("create_comment.html")
+
+
+def comment():
+    reviews = Reviews.query.order_by(Reviews.date.desc()).all()
+    return render_template("main_page.html", reviews=reviews)
+
+
+@app.route('/reviews/<int:id>/update', methods=['POST', 'GET'])
+def comment_update(id):
+    review = Reviews.query.get(id)
+    if request.method == "POST":
+        review.nickname = request.form['nickname']
+        review.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error, try again later'
+    else:
+        review = Reviews.query.get(id)
+        return render_template("update_comment.html", review=review)
+
+
+@app.route('/reviews/<int:id>/delete')
+def comment_delete(id):
+    review = Reviews.query.get_or_404(id)
+
+    try:
+        db.session.delete(review)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "Error while deleting"
+    return render_template("comment.html", review=review)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- models.py
+++ models.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 class Reviews(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(30),default='Unknown')
+    #mark = db.comlumn(db.Integer(1-5))
     text=db.Column(db.String(500),nullable=False)
     date=db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
 
