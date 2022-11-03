b14078dfcbd26ba5c98eba9acbb276ac649a904e
2pokek <alkuzmich6479@gmail.com>
2022-11-03 21:57:48 +0300

    2

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from models import db,Reviews
+
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3.db'
+
+db.init_app(app)
 
 @app.route('/')
 def main_page():
         return render_template('main_page.html')
+
+@app.route('/pricing')
+def pricing_page():
+        return render_template('pricing.html')
+
+@app.route('/about')
+def about_page():
+        return render_template('about.html')
 
 
 if __name__ == '__main__':
---
+++ models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Reviews(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(30),default='Unknown')
+    text=db.Column(db.String(500),nullable=False)
+    date=db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Reviews %r' % self.id
